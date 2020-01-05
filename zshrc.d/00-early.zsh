366b627ba68c3f6d3d38e570cbad1cc3c6637e12
Adrian Moennich <adrian@planetcoding.net>
2020-01-05 01:53:15 +0100

    Add /usr/share/zsh/site-functions to fpath

--- zshrc.d/00-early.zsh
+++ zshrc.d/00-early.zsh
@@ -7,3 +7,8 @@ bindkey -e
 ZSH_CACHE="/tmp/.zsh-${USER}-${ZSH_VERSION}"
 mkdir -p $ZSH_CACHE
 chmod 700 $ZSH_CACHE
+
+# Add the location of zsh-completions to fpath, since it's
+# not always included there by default.
+typeset -U fpath
+[[ -d /usr/share/zsh/site-functions ]] && fpath+=(/usr/share/zsh/site-functions)
